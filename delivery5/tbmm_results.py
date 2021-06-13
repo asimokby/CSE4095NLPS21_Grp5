05994b01a8b1542b23bd1da78a9a764d589422ab
lkrfnr <lkrfnr@gmail.com>
2021-06-13 16:52:51 +0300

    Model tested with sample sentences from TBMM corpus.

--- delivery5/tbmm_results.py
+++ delivery5/tbmm_results.py
@@ -3,7 +3,11 @@ import re
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow import keras
 import pickle
+import tensorflow as tf
+import warnings
+warnings.filterwarnings("ignore")
 
+print(str(tf.__version__))
 
 def clean_text(text):
     """This function will process the t1ext and clean it before extracting the collocations
@@ -41,32 +45,99 @@ def load_donem_data(donem_number):
 
     return donem_sentences
 
+def test_model():
+    
+    max_tokens = 52
+    model = keras.models.load_model('SA_lstm_with_w2v_embedd.h5')
+    
+    with open('turkish_tokenizer.pickle', 'rb') as f:
+        
+        neg_results_file = open("negative_results_w2v_donem27.results", "w")
+        pos_results_file = open("positive_results_w2v_donem27.results", "w")
+        
+        turkish_tokenizer = pickle.load(f)
+        donem_number = 27
+        donem_sentences = load_donem_data(donem_number)
+
+        neg_results = {}
+        pos_results = {}
+        
+        donem_results = {}
+        
+        for year, sentences in donem_sentences.items():
+            
+            print(f'Processing year: {year}')
+            tokens = turkish_tokenizer.texts_to_sequences(sentences)
+        
+            if len(tokens) == 0: continue
+        
+            tokens_pad = pad_sequences(tokens, maxlen=max_tokens)
+            res = model.predict(tokens_pad)
+        
+            pos = 0
+            neg = 0
+        
+            neg_results[year] = {}
+            pos_results[year] = {}
+
+            for idx, prob in enumerate(res):
+
+                if prob[0] > 0.5:
+                    pos+=1
+                    pos_results[year][idx] = sentences[idx]
+                    pos_results_file.write(f"{year},{sentences[idx]}\n")
+                    pos_results_file.flush()
+                else: 
+                    neg+=1
+                    neg_results[year][idx] = sentences[idx]
+                    neg_results_file.write(f"{year},{sentences[idx]}\n")
+                    neg_results_file.flush()
+            
+            pos_neg_count = {'pos':pos, 'neg':neg}
+            donem_results[year] = pos_neg_count
+        
+        neg_results_file.close()
+        pos_results_file.close()
+
+    with open(f'donem_{donem_number}_neg_results.pickle', 'wb') as f:
+        pickle.dump(neg_results, f)
+
+test_model()
+
+def load_results(pk_file_name):
+
+    tokenizer_file = open('turkish_tokenizer.pickle', "rb")
+    tokenizer = pickle.load(tokenizer_file)
+
+    pk_file = open(pk_file_name, "rb")
+    res = pickle.load(pk_file)
+    pk_file.close()
+    with open(str(pk_file_name)[0:(str(pk_file_name).rindex(".")-1)]+".results", "w") as results_file:
+        results_file.write(str(res))
+        results_file.flush()
+        results_file.close()
+
+"""load_results("donem_27_results_keras.pickle")
+load_results("donem_27_results_w2v.pickle")"""
 
 
-max_tokens = 52
-model = keras.models.load_model('SA_lstm_with_keras_embedd.h5')
-with open('turkish_tokenizer.pickle', 'rb') as f:
-    turkish_tokenizer = pickle.load(f)
 
 
-donem_number = 20
-donem_sentences = load_donem_data(donem_number)
+def test_with_sentence(sentence):
+    
+    max_tokens = 52
+    model = keras.models.load_model('SA_lstm_with_keras_embedd.h5')
+    
+    tokenizer_file = open('turkish_tokenizer.pickle', "rb")
+    tokenizer = pickle.load(tokenizer_file)
 
-donem_results = {}
-for year, sentences in donem_sentences.items():
-    print(f'Processing year: {year}')
-    tokens = turkish_tokenizer.texts_to_sequences(sentences)
-    if len(tokens) == 0: continue
+    tokens = tokenizer.texts_to_sequences(sentence)
+
     tokens_pad = pad_sequences(tokens, maxlen=max_tokens)
     res = model.predict(tokens_pad)
     pos = 0
     neg = 0
     for prob in res:
-        if prob[0] > 0.5: pos+=1
-        else: neg+=1
-    pos_neg_count = {'pos':pos, 'neg':neg}
-    donem_results[year] = pos_neg_count
+        if prob[0] > 0.5: return "positive"
+        else: return "negative"
 
-
-with open(f'donem_{donem_number}_results.pickle', 'wb') as f:
-    pickle.dump(donem_results, f)
